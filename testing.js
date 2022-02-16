5d44012c2a6a35198e7ac47e6e8f41cf47a278ae
Devin <devinborchard@gmail.com>
2022-02-16 13:00:55 -0500

    updated class and node file

---
+++ testing.js
@@ -0,0 +1,13 @@
+import {LinkedList} from './LinkedList.js'
+
+
+
+var list = new LinkedList()
+list.addVal(4)
+list.addVal(3)
+list.addVal(2)
+list.addVal(1)
+
+list.insert(0,3)
+
+list.printList()
